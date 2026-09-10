266ce7a49373aa55c1d2ad3eef73a6f83d22251d
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-09 22:00:17 -0300

    boll numero par

--- main.c
+++ main.c
@@ -1,26 +1,28 @@
 #include<stdio.h>
 #include<windows.h>
+#include<stdbool.h>
+
+bool ehPar(int numero){
+    return(numero % 2 == 0);
+
+}
 
 int main(){
 
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-//teste
+bool resultado1 = ehPar(4);
+bool resultado2 = ehPar(7);
+bool resultado3 = ehPar(11);
+bool resultado4 = ehPar(48);
+bool resultado5 = ehPar(8);
 
-int a = 5;
-int b = 4;
-//atribuindo
-
-
-printf("a soma é: %d\n", (a + b));
-
-printf("a subtração é: %d\n", (a - b));
-
-printf("a multiplicação é: %d\n", (a * b));
-
-printf("a divisão é: %d\n", (a / b));
-
+    printf("4 é par? %d\n", resultado1);
+    printf("7 é par %d\n", resultado2);
+    printf("11 é par? %d\n", resultado3);
+    printf("48 é par? %d\n", resultado4);
+    printf("8 é par? %d\n", resultado5);
 
 return 0;
 
