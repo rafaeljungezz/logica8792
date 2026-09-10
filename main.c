4e935dc251fa5a2958ba967a538f524512d320d3
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-09 21:26:25 -0300

    subtraçao, divisao e multiplicaçao

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+#include<windows.h>
 
 int main(){
+
+SetConsoleCP(65001);
+SetConsoleOutputCP(65001);
+
+
 
 int a = 5;
 int b = 4;
 //atribuindo
+
 
 printf("a soma é: %d\n", (a + b));
 
