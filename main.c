d293152bc6361fe5fb37b52b6bce47199df0919e
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-11 19:37:18 -0300

    sucessor e antecessor

--- main.c
+++ main.c
@@ -1,28 +1,19 @@
 #include<stdio.h>
 #include<windows.h>
-#include<stdbool.h>
 
-bool ehPar(int numero){
-    return(numero % 2 == 0);
-
-}
 
 int main(){
 
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-bool resultado1 = ehPar(4);
-bool resultado2 = ehPar(7);
-bool resultado3 = ehPar(11);
-bool resultado4 = ehPar(48);
-bool resultado5 = ehPar(8);
+int n;
 
-    printf("4 é par? %d\n", resultado1);
-    printf("7 é par %d\n", resultado2);
-    printf("11 é par? %d\n", resultado3);
-    printf("48 é par? %d\n", resultado4);
-    printf("8 é par? %d\n", resultado5);
+printf("Digite um número:");
+scanf("%d", &n);
+
+printf("Antecessor: %d\n", n - 1);
+printf("Sucessor: %d\n", n + 1);
 
 return 0;
 
