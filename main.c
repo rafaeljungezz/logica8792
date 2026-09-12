a7b3c6d5d403768a34498355694b2b0f11e72f46
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-11 21:16:47 -0300

    média de números

--- main.c
+++ main.c
@@ -7,14 +7,14 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-float raio;
-const float PI = 3.14159;
+float n1, n2, n3, media;
 
-printf("Digite o raio do círculo: ");
-    scanf("%f", &raio);
-printf("Area: %.2f\n", PI * raio * raio);
-printf("perímetro: %.2f\n", 2 * PI * raio);
+printf("Digite três notas: ");
+scanf("%f %f %f", &n1, &n2, &n3);
 
+media= (n1 + n2 + n3) / 3;
+
+printf("média %.2f\n", media);
 
 return 0;
 
