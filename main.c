3ad2e0d40ec0e556c8d005bcedd9f1e0b6ab80c4
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-11 21:43:51 -0300

    horas, minutos, segundos e resto

--- main.c
+++ main.c
@@ -7,13 +7,19 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-float celsius, fahrenheit;
 
-printf("digite a temperatura em celsius: ");
-scanf("%f", &celsius);
+int segundos, horas, minutos, resto;
 
-fahrenheit = (celsius * 9.0 / 5.0) + 32;
-printf("fahrenheit: %.2f\n", fahrenheit);
+    printf("DIgite a quantidade de seguntos: ");
+    scanf("%d", &segundos);
+
+    horas = segundos / 3600;
+    minutos = (segundos % 3600) / 60;
+    resto = segundos % 60;
+
+    printf("%d horas, %d minutos e %d segundos\n", horas, minutos, resto);
+
+
 
 return 0;
 
