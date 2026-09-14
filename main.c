ade58edbad426d801d00822582f333d55b200738
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-14 19:48:14 -0300

    maior e menor de idade

--- main.c
+++ main.c
@@ -8,15 +8,18 @@ SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
 
-int n;
+int idade;
 
-    printf("digite um número: ");
-    scanf("%d", &n);
+printf("digite uma idade: ");
+scanf("%d", &idade);
 
+if(idade >= 18){
+    printf("você é maior de idade!");
+}else{
+    printf("você é menor de idade!");
+}
 
-printf("%s", n % 2 == 0 ? "\nPar" : "\nImpar");
 
 return 0;
 
 }
-
