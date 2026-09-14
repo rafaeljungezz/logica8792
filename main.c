6b248a6b86af33b7e53a8ce88a0e18eaf58f8b38
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-14 20:14:13 -0300

    senior, maior de idade, adolescente e pre-adolescente

--- main.c
+++ main.c
@@ -10,13 +10,21 @@ SetConsoleOutputCP(65001);
 
 int idade;
 
+
+
 printf("digite uma idade: ");
 scanf("%d", &idade);
 
-if(idade >= 18){
-    printf("você é maior de idade!");
+if(idade >= 50){
+     printf("você é um sênior!");
+}else if(idade >= 18){
+    printf("Você é um maior de idade!");
+}else if(idade >= 12){
+    printf("Você é um adolescente!");
+}else if(idade >= 9){
+    printf("você é um pré-adolescente");
 }else{
-    printf("você é menor de idade!");
+    printf("voçê é uma criança");
 }
 
 
