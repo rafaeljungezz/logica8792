7bf9d2198e3b20f1059a72dd819ddab9c067fd08
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-16 21:41:11 -0300

    lados do triângulo

--- main.c
+++ main.c
@@ -7,27 +7,20 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
+float a, b, c;
 
-float n1, n2, n3, media;
+printf("Digite os três lados do triângulo: ");
+scanf("%f %f %f", &a, &b, &c);
 
-printf("Digite a nota 1: ");
-scanf("%f", &n1);
-
-printf("Digite a nota 2: ");
-scanf("%f", &n2);
-
-printf("Digite a nota 3: ");
-scanf("%f", &n3);
-
-media = (n1 + n2 + n3) / 3;
-
-if(media >= 7){
-    printf("Aprovado!");
-}else if(media >= 5){
-    printf("recuperação!");
+if( a == b && b == c){
+    printf("Equilátero!\n");
+}else if(a == b || a == c || b == c){
+    printf("Isóceles!\n");
 }else{
-    printf("reprovado!");
+    printf("Escaleno!\n");
 }
+
+
 
 
 return 0;
