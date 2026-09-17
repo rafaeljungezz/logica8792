3fb5def8194e20a88bda24b747041362cc24be05
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-16 22:01:04 -0300

    carro de passeio e moto tarifas

--- main.c
+++ main.c
@@ -7,17 +7,27 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-float a, b, c;
+int opção;
 
-printf("Digite os três lados do triângulo: ");
-scanf("%f %f %f", &a, &b, &c);
+printf("--- SISTEMA DE PEDÁGIO ---\n");
+printf("1 - Moto\n");
+printf("2- Carro de passeio\n");
+scanf("%d", e &opção);
 
-if( a == b && b == c){
-    printf("Equilátero!\n");
-}else if(a == b || a == c || b == c){
-    printf("Isóceles!\n");
-}else{
-    printf("Escaleno!\n");
+switch(opção);
+{
+    case 1:
+        printf("Categoria: Moto | Tarifa: R$5,00\n")
+        break;
+    
+    case 2:
+        printf("categoria: Carro de passeio | Tarifa; R$10,00\n");
+        break;
+
+    default:
+        printf(" Erro: categoria nâo encontrada no sistema!\n");
+        break;
+
 }
 
 
