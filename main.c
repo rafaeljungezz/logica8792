7670767a621258919d24b49ff5f4af60db3c42ab
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-16 21:15:20 -0300

    nota de aprovação, recuperação e reprovação

--- main.c
+++ main.c
@@ -7,44 +7,18 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int diaDaSemana;
 
-int dia; 
-printf("digite um número (de 1 a 7): ");
-scanf("%d", &dia);
+int nota;
 
-switch(dia){
-    case 1:
-        printf("Domingo!");
-        break;
+printf("digite a nota do aluno: ");
+scanf("%d", &nota);
 
-    case 2:
-        printf("Segunda-feira!");
-        break;
-
-    case 3:
-        printf("Terça-feira!");
-        break;
-
-    case 4:
-        printf("Quarta-feira!");
-        break;
-
-    case 5:
-        printf("Quinta-feira!");
-        break;
-
-    case 6:
-        printf("Sexta-feira!");
-        break;
-
-    case 7:
-        printf("Sábado!");
-        break;
-        
-    default:
-        printf("Número inválido!");
-        break;
+if(nota >= 7){
+    printf("Aprovado!");
+}else if(nota >= 5){
+    printf("Recuperação!");
+}else{
+    printf("Reprovado!");
 }
 
 return 0;
