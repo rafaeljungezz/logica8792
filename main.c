517b3903acb478c03804294eae6d0a9ced839bc0
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-18 21:05:14 -0300

    multiplos de 3 e 5

--- main.c
+++ main.c
@@ -7,16 +7,19 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-char letra;
-printf("digite uma letra: ");
-scanf("%c", &letra);
+int n;
+printf("digite um numero: ");
+scanf("%d", &n);
 
-if(letra == 'a' || letra == 'e' || letra =='i' || letra == 'o' || letra == 'u' ||
- letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
-    printf("vogal\n");
- }else{
-    printf("consoante\n");
- }
+if(n % 3 == 0 && n % 5 == 0){
+    printf("multiplo de 3 e 5\n");
+}else if(n % 3== 0){
+    printf("multiplo de 3\n");
+}else if(n % 5 == 0){
+    printf("multilpo de 5\n");
+}else{
+    printf("não é multiplo de 3 nem de 5\n");
+}
 
 
 
