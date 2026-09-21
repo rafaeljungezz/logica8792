81764f4940b36e3278548e636b68e9dd588fe6f0
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-21 19:42:54 -0300

    for de 0 a 10000

--- main.c
+++ main.c
@@ -7,25 +7,12 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-float altura, peso, imc;
-
-printf("digite o peso (kg): ");
-scanf("%f", &peso);
-
-printf("digite a altura (m): ");
-scanf("%f", &altura);
-
-imc = peso / (altura * altura);
-
-if(imc < 18.5){
-    printf("classificaçao: abaixo do peso\n");
-}else if(imc < 25){
-    printf("classificaçao: peso normal\n");
-}else if(imc < 30){
-    printf("classificaçao: acima do peso\n");
-}else{
-    printf("classificaçao: obesidade\n");
+for(int i = 0; i < 10000; i++){
+    printf("%d\n", i);
+    i++;
 }
+
+printf("meu for funcionou!\n");
 
 
 
