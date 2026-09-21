eb62adb376cd08af01d4bb13924b29992586033d
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-21 20:06:10 -0300

    array com nomes

--- main.c
+++ main.c
@@ -7,13 +7,14 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-char frutas[3][20] = {"\nmaçã", "\nbanana", "\nlaranja"};
+char nomes[6][50] = {"\nRafael", "\nOtavio", "\nMatheus", "\nErick", "\nFlavio", "\njose"};
 
-printf("%s", frutas[0]);
-printf("%s", frutas[1]);
-printf("%s", frutas[2]);
-
-
+printf("%s", nomes[0]);
+printf("%s", nomes[1]);
+printf("%s", nomes[2]);
+printf("%s", nomes[3]);
+printf("%s", nomes[4]);
+printf("%s", nomes[5]);
 
 return 0;
 
