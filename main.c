2e3c8a06e63745f6ba546f845ad70b40b11b1333
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-21 19:57:39 -0300

    array com frutas

--- main.c
+++ main.c
@@ -7,12 +7,11 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-for(int i = 0; i < 10000; i++){
-    printf("%d\n", i);
-    i++;
-}
+char frutas[3][20] = {"\nmaçã", "\nbanana", "\nlaranja"};
 
-printf("meu for funcionou!\n");
+printf("%s", frutas[0]);
+printf("%s", frutas[1]);
+printf("%s", frutas[2]);
 
 
 
