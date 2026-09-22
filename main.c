81794e323c9555b35607f6d848c01b9ee544a996
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-21 21:48:38 -0300

    arrey declarando as variaveis sem chaves

--- main.c
+++ main.c
@@ -7,11 +7,13 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int numeros[3] = {1, 2, 3};
+int numeros[3];
 
 numeros[0] = 10;
+numeros[1] = 20;
+numeros[2] = 30;
 
-printf("%d", numeros[0]);
+printf("%d", numeros[2]);
 
 return 0;
 
