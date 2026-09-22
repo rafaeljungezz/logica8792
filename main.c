0314ee4a8ebd33e285146dec7677dca47db4e8ab
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-21 21:05:11 -0300

    array com idades

--- main.c
+++ main.c
@@ -7,37 +7,11 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int numerosInteiros[6] = {1, 5, 10, 15, 20, 25};
-float numerosFloats[5] = {10.5, 20.8, 30.2, 40.9, 50.1};
-char celulares[5][50] = {"motorola", "samsung", "apple", "xiaome", "huawei"};
+int idades[3] = {15, 45, 67};
 
-
-printf("\nNúmeros inteiros:\n ");
-
-    printf("%d\n", numerosInteiros[0]);
-    printf("%d\n", numerosInteiros[1]);
-    printf("%d\n", numerosInteiros[2]);
-    printf("%d\n", numerosInteiros[3]);
-    printf("%d\n", numerosInteiros[4]);
-    printf("%d\n", numerosInteiros[5]);
-
-printf("\nNúmeros decimais:\n ");
-
-    printf("%f\n", numerosFloats[0]);
-    printf("%f\n", numerosFloats[1]);
-    printf("%f\n", numerosFloats[2]);
-    printf("%f\n", numerosFloats[3]);
-    printf("%f\n", numerosFloats[4]);
-
-printf("\nCelulares:\n\n ");
-
-    printf("%s\n", celulares[0]);
-    printf("%s\n", celulares[1]);
-    printf("%s\n", celulares[2]);
-    printf("%s\n", celulares[3]);
-    printf("%s\n", celulares[4]);
-
-    
+printf("%s %d %s", "Rafael tem, ", idades[0], "anos!\n");
+printf("%s %d %s", "Flavio tem, ", idades[2], "anos!\n");
+printf("%s %d %s", "jose tem, ", idades[1], "anos!\n");
 
 return 0;
 
