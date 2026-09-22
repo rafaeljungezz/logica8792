8dd1f157c16d0452370b29c6487b9abf8e3fa95a
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-21 21:35:09 -0300

    soma com for

--- main.c
+++ main.c
@@ -7,11 +7,22 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-for(int i = 100; i >=0; i = i -10){
-    printf("%d\n", i);
+int soma = 0;
+int i;
+
+for(i = 1; i <= 5; i++){
+//a coluna da direita é o "i", e o da esquerda é a "soma"
+
+    //0 + 1
+    //1 + 2
+    //3 + 3
+    //6 + 4
+    //10 + 5
+
+    soma = soma + i;
 }
 
-
+printf("soma é %d", soma);
 
 return 0;
 
