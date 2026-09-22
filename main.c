67f2cd7f81df066cb8451d89f4c1107c9bd82949
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-22 20:48:58 -0300

    função com soma, subtração, multiplicação e divisão

--- main.c
+++ main.c
@@ -9,6 +9,18 @@ int subtrair(int a, int b){
     return a - b;
 }
 
+int multiplicar(int a, int b){
+    return a * b;
+}
+
+int dividir(int a, int b){
+    if(b <=0){
+        printf("Ele precisa ser maior que 0!");
+    }else{
+        return a / b;
+    }
+}
+    
 
 int main(){
 
@@ -21,6 +33,11 @@ printf("o resultado da soma é: %d\n", resultado);
 int resultadosubtraçao = subtrair(30, 12);
 printf("o resultado da subtração é: %d\n", resultadosubtraçao);
 
+int resultadomultiplicar= multiplicar(2, 20);
+printf("o resultado da multiplicação é: %d\n", resultadomultiplicar);
+
+int resultadodivisao = dividir(3, 0);
+printf("o resultado da divisão é: %d\n", resultadodivisao);
 
 return 0;
 
