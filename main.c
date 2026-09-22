e1b886cc1c218d46bc8e6d2d1a7642a6a588fba2
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-21 21:58:18 -0300

    lenght

--- main.c
+++ main.c
@@ -7,13 +7,13 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int numeros[3];
+int numeros[] = {10, 20, 30};
 
-numeros[0] = 10;
-numeros[1] = 20;
-numeros[2] = 30;
+printf("%zu", sizeof(numeros));
+int lenght = sizeof(numeros) / sizeof(numeros)[0];
 
-printf("%d", numeros[2]);
+printf("%d", lenght);
+
 
 return 0;
 
