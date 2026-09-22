ce21bb2341e1655c3463d79a3fd6d86a7a8ecde4
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-22 20:09:11 -0300

    função com soma e subtração

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 #include<Windows.h>
 
-void saudar(const char *nome){
-    printf("olá %s\n", nome);
+int somar(int a, int b){
+    return a + b;
 }
+
+int subtrair(int a, int b){
+    return a - b;
+}
+
 
 int main(){
 
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-saudar("Rafael");
-saudar("Junges");
-saudar("Trisch");
+int resultado = somar(45, 45);
+printf("o resultado da soma é: %d\n", resultado);
 
+int resultadosubtraçao = subtrair(30, 12);
+printf("o resultado da subtração é: %d\n", resultadosubtraçao);
 
 
 return 0;
