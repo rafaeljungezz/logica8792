d6b343aa1f1ca598ff3282317567648c90bcd70d
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-22 21:53:12 -0300

    nome completo ( com sizeof e stdin )

--- main.c
+++ main.c
@@ -7,14 +7,12 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int numero;
-printf("digite um numero: ");
-scanf("%d", &numero);
+char nome[50];
 
-int quadrado = (int)pow(numero, 2);
-int cubo = (int)pow(numero, 3);
-printf("quadrado de %d = %d\n", numero, quadrado);
-printf("cubo de %d = %d", numero, cubo);
+printf("digite seu nome completo aqui:");
+fgets(nome, sizeof(nome), stdin); // lê até encontrar ENTER
+
+printf("olá %s", nome);
 
 
 return 0;
