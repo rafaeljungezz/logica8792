7da8f2acfdc55b987be16a61a55c68f287a54c1b
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-23 20:39:50 -0300

    tabuada com funçao( com int e com opçao de escolher o numero digitado

--- main.c
+++ main.c
@@ -4,12 +4,11 @@
     
 int tabuada(int n){
     int resultado;
-    printf("tabuada do %d:\n", n);
-    for(int i = 1; i <= 10; i++){
-        resultado = n * i;
-        printf("%d x %d = %d\n", n, i, resultado);
+        printf("tabuada do %d:\n", n);
+            for(int i = 1; i <= 10; i++){
+            printf("%d x %d = %d\n", n, i, n * i);
     }
-    return resultado;
+         return resultado;
 }
 
 
@@ -20,8 +19,10 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int ultimo = tabuada(14);
-printf("ultimo valor da tabuada: %d\n", ultimo);
+int numero;
 
+    printf("digite um numero para ver a tabuada: ");
+    scanf("%d", &numero);
+    tabuada(numero);
 
 }
