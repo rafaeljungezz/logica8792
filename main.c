96a67e4e1d8c2d0149590ec2e68f2387936b3b17
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-23 20:03:48 -0300

    tabuada com funçao

--- main.c
+++ main.c
@@ -2,17 +2,11 @@
 #include<Windows.h>
 #include<math.h>
     
-void fibonacci(int rafael){
-    int a = 0, b = 1, c;
-    printf("sequência de fibonacci (%d rafael): \n", rafael);
-    for(int i = 1; i <= rafael; i++){
-        printf("%d\n", a);
-        c = a + b;
-        a = b;
-        b = c;
+void tabuada(int n){
+    printf("tabuada do %\n", n);
+    for(int i = 1; i <= 10; i++){
+        printf(" %d * %d = %d\n", n, i, n * i);
     }
-   
-    
 }
 
 
@@ -23,7 +17,7 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-fibonacci(10);
+tabuada(12);
 
 
 }
