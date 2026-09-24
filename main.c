317d5519f00f46c557ca9c4543227e35d6810cee
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-23 21:48:47 -0300

    maxMin

--- main.c
+++ main.c
@@ -4,31 +4,25 @@
 #include<string.h>
 
 
-void contarVogais(char palavra[]){
-    int contador = 0;
-    for(int i = 0; i <strlen(palavra); i++){
-        char c = palavra[i];
-        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
-            contador++;
-        }//strlen calcula os caracteres de uma palavra.
+void maxMin(int v[], int tamanho){
+    int max = v[0], min =v[0];
+    for(int i = 1; i< tamanho; i++){
+        if(v[i] > max) max = v[i];
+        if(v[i] < min) min = v[i];
+    }
+    printf("máximo: %d, mínimo: %d\n", max, min);
 
-     }
-    printf("a palavra '%s' tem %d vogais.\n", palavra, contador);
 }
+
 
 int main(){
 
-SetConsoleCP(65001);
-SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 
-char palavra;
+int numeros[] = {42, 38, 25, 155, 56};
 
-    printf("digite uma palavra: ");
-    scanf("%s", &palavra);
-    contarVogais(&palavra);
-
-
+    maxMin(numeros, 5);
 
     return 0;
 }
