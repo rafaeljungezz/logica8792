b1ec6e4f71753ee7da6d2bf00bf3a6d9067d815b
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-23 21:54:53 -0300

    se o numero é par ou ímpar (sem função)

--- main.c
+++ main.c
@@ -4,25 +4,21 @@
 #include<string.h>
 
 
-void maxMin(int v[], int tamanho){
-    int max = v[0], min =v[0];
-    for(int i = 1; i< tamanho; i++){
-        if(v[i] > max) max = v[i];
-        if(v[i] < min) min = v[i];
-    }
-    printf("máximo: %d, mínimo: %d\n", max, min);
-
-}
-
-
 int main(){
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-int numeros[] = {42, 38, 25, 155, 56};
+int n;
 
-    maxMin(numeros, 5);
+printf("digite um número: ");
+scanf("%d", &n);
+
+if(n % 2 == 0){
+    printf("\nPar");
+}else{
+    printf("\nímpar");
+}
 
     return 0;
 }
