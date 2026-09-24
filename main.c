e28e9661a541c2806b79d5a243a1f6f391c1a121
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-24 19:49:02 -0300

    while com opçoes(switch e cases)

--- main.c
+++ main.c
@@ -9,15 +9,38 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int numero;
+   int opcao = -1;
 
-        printf("digite um numero positivo: ");
-        scanf("%d", &numero);
+   while(opcao != 0){
+    printf("\nmenu: \n");
+    printf("1 - depositar\n");
+    printf("2 - sacar\n");
+    printf("3 - consultar\n");
+    printf("0 - sair\n");
+    printf("escolha: ");
+    scanf("%d", &opcao);
 
-    while(numero <= 0){
-        printf("numero invalido! digite novamente: ");
-        scanf("%d", &numero);
-    }
-        printf("numero valido: %d\n", numero);
+    switch(opcao){
+        case 1:
+            printf("voce escolheu depositar\n");
+            break;
+
+        case 2:
+            printf("voce escolheu sacar\n ");
+            break;
+
+        case 3:
+            printf("voce escolheu consultar\n ");
+            break;
+
+        case 0:
+            printf("saino...\n");
+            break;
+
+        default:
+        printf("POÇAO INVALIDA!\n ");
+
+        }
+   }
 
 }
