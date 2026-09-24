e3934d11022feda10f0cff93e67aaa81e5eb9be5
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-24 20:17:35 -0300

    while com soma de nuemros

--- main.c
+++ main.c
@@ -9,38 +9,16 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int opcao = -1;
+   int numero, soma = 0;
+    
+        printf("digite um numero para somar (0 encerra): ");
+        scanf("%d", &numero);
 
-   while(opcao != 0){
-    printf("\nmenu: \n");
-    printf("1 - depositar\n");
-    printf("2 - sacar\n");
-    printf("3 - consultar\n");
-    printf("0 - sair\n");
-    printf("escolha: ");
-    scanf("%d", &opcao);
+    while(numero != 0){
+        soma += numero;
+        scanf("%d", &numero);
+    }
+printf("soma total = %d\n", soma);
 
-    switch(opcao){
-        case 1:
-            printf("voce escolheu depositar\n");
-            break;
-
-        case 2:
-            printf("voce escolheu sacar\n ");
-            break;
-
-        case 3:
-            printf("voce escolheu consultar\n ");
-            break;
-
-        case 0:
-            printf("saino...\n");
-            break;
-
-        default:
-        printf("POÇAO INVALIDA!\n ");
-
-        }
-   }
-
+    return 0;
 }
