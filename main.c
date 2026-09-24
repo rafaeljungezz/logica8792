376640310d9afd5762ae8abf443f89d0d51ebcc3
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-24 19:21:11 -0300

    while com numero valido ou invalido

--- main.c
+++ main.c
@@ -9,14 +9,15 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int i = 1;
+   int numero;
 
-    while(i <= 5){
-        printf("%d\n ", i);
-        i++;
+        printf("digite um numero positivo: ");
+        scanf("%d", &numero);
+
+    while(numero <= 0){
+        printf("numero invalido! digite novamente: ");
+        scanf("%d", &numero);
     }
-
-()
-
+        printf("numero valido: %d\n", numero);
 
 }
