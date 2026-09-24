4ad7b5adc4c1a040eb85ca59f53d2cf87e819c23
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-23 21:26:16 -0300

    contador de vogais com funçao

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 #include<Windows.h>
 #include<math.h>
-    
-void linha(int tamanho){
-    for(int i = 0; i < tamanho; i++){
-        printf(":)");
-    }
-    printf("\n");
+#include<string.h>
+
+
+void contarVogais(char palavra[]){
+    int contador = 0;
+    for(int i = 0; i <strlen(palavra); i++){
+        char c = palavra[i];
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            contador++;
+        }//strlen calcula os caracteres de uma palavra.
+
+     }
+    printf("a palavra '%s' tem %d vogais.\n", palavra, contador);
 }
 
 int main(){
@@ -14,7 +22,13 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-linha(2000);
+char palavra;
+
+    printf("digite uma palavra: ");
+    scanf("%s", &palavra);
+    contarVogais(&palavra);
 
 
+
+    return 0;
 }
