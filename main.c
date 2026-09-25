4a957d81f3e2babdf06e935314a747d16f485358
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-24 21:32:02 -0300

    desafio com tabuada com FOR e dizendo o valor invalido em casos de numeros

--- main.c
+++ main.c
@@ -10,17 +10,21 @@ int main(){
     SetConsoleOutputCP(65001);
 
    int n;
+   int resultado;
+
 
     printf("digite um número: ");
-    scanf("%d", &n);
+    resultado = scanf("%d", &n);
+
+    if(resultado != 1){
+        printf("entrada inválida!por favor, digite apenas numeros! ");
+        return 1;
+    }
 
     for(int i = 1; i <= 10; i++){
-
-        printf(" %d * %d = %d\n", n, i, n * i);
-
+        printf("%d * %d = %d\n", n, i, n * i);
     }
-    
-    
+      
 
 
     return 0;
