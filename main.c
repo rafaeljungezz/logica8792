568fee2533367e8684c1fba515c9b1d168402fc2
rafaeljungezz <jungesrafinhajunges@gmail.com>
2026-09-25 21:27:47 -0300

    escolhendo a quantidade de alunos e tirando a media das notas

--- main.c
+++ main.c
@@ -9,19 +9,18 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-  int n, numeroInvertido = 0, resto; // n é o nmero digitado pelo usuario. //numeroInvertido é o numero que que começa em 0 e vai virando nosso nuemro invertido.
-                                    // resto é para puxar o ultimo numero do nuemro escolhido
+    int n;
+    float nota, soma = 0;
 
-    printf("digite um numero: "); //98
-    scanf("%d", & n); // o numero é armazenado em n
+        printf("quantos alunos tem a turma: ");
+        scanf("%d", &n);
 
-    while(n != 0){ 
-        resto = n % 10;
-        numeroInvertido = numeroInvertido * 10 + resto;
-        n = n / 10;
-    }
-    
-    printf("seu numero invertido é: %d\n", numeroInvertido); // omde sai o numero invertido no terminal.
+        for(int i = 0; i < n; i++){
+            printf("digite a nota do aluno %d: \n", i + 1);
+            scanf("%f", &nota);
+            soma += nota;
+        }
+        printf("média da turma: %.2f\n", soma / n);
 
     return 0;
 
